0cace0770162147bf77ad839f8812d2da4df7e1b
Dallin Magleby <13580051+dallinlmag@users.noreply.github.com>
2026-02-24 19:24:16 -0800

    Update Sample stuff
    Comment out the sample stuff so that it builds in Github.
    
    update sample
    
    Fix refs

--- src/settings.ts
+++ src/settings.ts
@@ -1,15 +1,15 @@
 import {App, PluginSettingTab, Setting} from "obsidian";
 import MyPlugin from "./main";
 
-export interface MyPluginSettings {
+export interface PluginSettings {
 	mySetting: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
+export const DEFAULT_SETTINGS: PluginSettings = {
 	mySetting: 'default'
 }
 
-export class SampleSettingTab extends PluginSettingTab {
+export class PTTSettings extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
